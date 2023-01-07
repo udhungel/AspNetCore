e4728a5e6a5ce5ac236e4e0163885ed42fd07238
udhungel <udeepdhungel@gmail.com>
2023-01-07 20:13:47 +0100

    Image tag helper in asp net mvc core Note: A unique hash value is generated and appended to the URL Each time the image in Server changes a new has value is calculated and cached

--- AspNetCore/Controllers/HomeController.cs
+++ AspNetCore/Controllers/HomeController.cs
@@ -25,7 +25,7 @@ namespace EmployeeManagement_AspNetCore.Controllers
         {
             HomeDetailsViewModel modelviewModel = new HomeDetailsViewModel()
             {
-                Employee = _employeeRepository.GetEmployee(id),
+                Employee = _employeeRepository.GetEmployee(1),
                 PageTitle = "Employee Details PageTitle"
             };           
             
