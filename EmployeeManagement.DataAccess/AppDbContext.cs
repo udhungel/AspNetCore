3d1068ea454d9b9280c48d2d56ef1525dc591c86
udhungel <udeepdhungel@gmail.com>
2023-01-09 20:09:50 +0100

    Adding DbContext in entity framework core

---
+++ EmployeeManagement.DataAccess/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using EmployeeManagement_AspNetCore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.DataAccess
+{
+    public class AppDbContext : DbContext 
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        } 
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
