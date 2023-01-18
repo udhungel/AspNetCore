3f057cd69d1c4007e5f107c43ec4cd19776b7507
udhungel <udeepdhungel@gmail.com>
2023-01-18 23:36:36 +0100

    Handling 404 Errors for Details View Id Note: Pass the Id so that you can show it in the View with a message

--- AspNetCore/Controllers/HomeController.cs
+++ AspNetCore/Controllers/HomeController.cs
@@ -29,11 +29,20 @@ namespace EmployeeManagement_AspNetCore.Controllers
             return View(result);
             
         }
-        public ViewResult Details(int id)
+        public ViewResult Details(int? id)
         {
+            Employee employee = _employeeRepository.GetEmployee(id.Value);
+            if (employee == null)
+            {
+                Response.StatusCode = 404;
+                return View("EmployeeNotFound", id.Value); 
+
+            }
+
+            
             HomeDetailsViewModel modelviewModel = new HomeDetailsViewModel()
             {
-                Employee = _employeeRepository.GetEmployee(id),
+                Employee = employee,
                 PageTitle = "Employee Details PageTitle"
             };           
             
