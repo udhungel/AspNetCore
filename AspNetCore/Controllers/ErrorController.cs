c47f3508fce9e1f39bd70857aaaa11902cf13896
udhungel <udeepdhungel@gmail.com>
2023-01-20 18:20:57 +0100

    Logging Exception in asp net core Note: Inject Ilogger Service , specify the type as a arugument for us its - ErrorController ILooger service instance we can use LogInformation, LogWarning or LogError

--- AspNetCore/Controllers/ErrorController.cs
+++ AspNetCore/Controllers/ErrorController.cs
@@ -1,17 +1,29 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace EmployeeManagement_AspNetCore.Controllers
 {
     public class ErrorController : Controller
     {
+        private readonly ILogger<ErrorController> logger;
+
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            this.logger = logger;
+        }
+
+
+        // this is when no route matches 
         [Route("Error/{StatusCode}")]
         public IActionResult HttpStatusCodeHandler(int statusCode)
         {
+            var statusCodeResult = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
             switch (statusCode)
             {
-                case 404: ViewBag.ErrorMessage = "Sorry, the resource you requested could not be found"; 
+                case 404: ViewBag.ErrorMessage = "Sorry, the resource you requested could not be found";
+                    logger.LogWarning($"404 Error Occured. Path =  {statusCodeResult.OriginalPath}" + $" and Querystring {statusCodeResult.OriginalQueryString}");
                     break;
             }
             return View("NotFound");
@@ -22,6 +34,7 @@ namespace EmployeeManagement_AspNetCore.Controllers
         public IActionResult Error() 
         {
             var exceptionDetails = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            logger.LogError($"This path {exceptionDetails.Path} threw an exception" + $"{exceptionDetails.Error}");
             ViewBag.ExceptionPath = exceptionDetails.Path;
             ViewBag.ExceptionMessage = exceptionDetails.Error.Message;
             ViewBag.StackTrace = exceptionDetails.Error.StackTrace;
